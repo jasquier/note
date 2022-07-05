df00a2ab6571a700be54fb1ebb4605bdc7c2812b
John A. Squier <jasquier@gmail.com>
2022-07-05 15:38:21 -0400

    Upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
   rules: {},
