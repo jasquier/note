c5a85289c71613eb912a8c979ccb5457bcd1eab6
John A. Squier <jasquier@gmail.com>
2024-09-29 06:20:35 -0400

    Correct path to db

--- src/connectDb.ts
+++ src/connectDb.ts
@@ -2,11 +2,7 @@ import path from "node:path";
 import sqlite3, { Database } from "sqlite3";
 
 let db: Database;
-const dbPath: string = path.join(
-  "/Users/jasquier/dev/js/note",
-  "data",
-  "data.db"
-);
+const dbPath: string = path.join("/Users/jasquier/dev/note", "data", "data.db");
 
 export function connectDb(databasePath: string = dbPath): Database {
   if (!db) db = new sqlite3.Database(databasePath);
